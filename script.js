6dfe6bb779542d392b4506f3d206bf71eef11256
Daniel Roque <droque6@icloud.com>
2023-09-21 23:59:21 -0500

    Fix all calculations

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ function calculateRefundIndividual() {
     startDate = new Date(startDate);
     endDate = new Date(endDate);
 
-    var monthsDifference = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
-   
-    var amountUsed = monthsDifference * 12.9166667;
+    var yearsDifference = endDate.getFullYear() - startDate.getFullYear();
+    var monthsDifference = endDate.getMonth() - startDate.getMonth();
+    var daysDifference = endDate.getDate() - startDate.getDate();
+    
+    // Calculate total time in months rounded up
+    var totalTime = (yearsDifference * 12) + monthsDifference + Math.ceil(daysDifference / 30);
+
+    var amountUsed = totalTime * 12.9166667;
     var refundAmount= (annualFee - amountUsed);
 
     document.getElementById('amountUsed').textContent = amountUsed.toFixed(2);
@@ -23,9 +28,14 @@ function calculateRefundCouple() {
     startDate1 = new Date(startDate1);
     endDate1 = new Date(endDate1);
 
-    var monthsDifference = (endDate1.getFullYear() - startDate1.getFullYear()) * 12 + (endDate1.getMonth() - startDate1.getMonth());
+    var yearsDifference1 = endDate1.getFullYear() - startDate1.getFullYear();
+    var monthsDifference1 = endDate1.getMonth() - startDate1.getMonth();
+    var daysDifference1= endDate1.getDate() - startDate1.getDate();
+    
+    // Calculate total time in months rounded up
+    var totalTime1 = (yearsDifference1 * 12) + monthsDifference1 + Math.ceil(daysDifference1 / 30);
 
-    var amountUsed1 = monthsDifference * 24.1666667;
+    var amountUsed1 = totalTime1 * 24.1666667;
     var refundAmount1 = (annualFee1 - amountUsed1);
 
     document.getElementById('amountUsed1').textContent = amountUsed1.toFixed(2);
@@ -40,9 +50,14 @@ function calculateRefundFamily() {
     startDate2 = new Date(startDate2);
     endDate2 = new Date(endDate2);
 
-    var monthsDifference = (endDate2.getFullYear() - startDate2.getFullYear()) * 12 + (endDate2.getMonth() - startDate2.getMonth());
+    var yearsDifference2 = endDate2.getFullYear() - startDate2.getFullYear();
+    var monthsDifference2 = endDate2.getMonth() - startDate2.getMonth();
+    var daysDifference2= endDate2.getDate() - startDate2.getDate();
+    
+    // Calculate total time in months rounded up
+    var totalTime2 = (yearsDifference2 * 12) + monthsDifference2 + Math.ceil(daysDifference2 / 30);
 
-    var amountUsed2 = monthsDifference * 26.6666667;
+    var amountUsed2 = totalTime2 * 26.6666667;
     var refundAmount2 = (annualFee2 - amountUsed2);
     
     document.getElementById('amountUsed2').textContent = amountUsed2.toFixed(2);
